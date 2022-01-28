0c0b95dd7405c961b63464bc4d79f9015e3a0419
blangwell <b.langwell@outlook.com>
2022-01-27 18:40:28 -0800

    Add SumAll function and test

--- arrays-and-slices/sum.go
+++ arrays-and-slices/sum.go
@@ -1,9 +1,21 @@
 package main
 
+// Takes an array of ints and returns the sum
 func Sum(numbers []int) int {
 	sum := 0
 	for _, number := range numbers {
 		sum += number
 	}
 	return sum
+}
+
+// Takes multiple arrays and returns their sums separately
+func SumAll(numbersToSum ...[]int) []int {
+	lengthOfNumbers := len(numbersToSum)
+	sums := make([]int, lengthOfNumbers)
+
+	for i, numbers := range numbersToSum {
+		sums[i] = Sum(numbers)
+	}
+	return sums
 }
